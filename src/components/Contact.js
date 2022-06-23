02b3639b487b023801395485c2aea4ff3e094b94
Hendry Gunawan <hendry_wu1984@yahoo.com>
2022-06-23 13:40:26 +0700

    Hendry Gunawan 152235865101-334 done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,24 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+// import contact from '../data/contacts.json';
 
-const Contact = () => {
+const Contact = (props) => {
+    const {data} = props;
+
     return (
-        <></>
+        <div className="card center">
+            <div className='cardFoto'>
+                <img src={data.photo}></img>
+            </div>
+            <div className="cardDetail">
+                <h4>{data.name}</h4>
+                <br></br>
+                <p>{data.phone}</p>
+                <br></br>
+                <p>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
